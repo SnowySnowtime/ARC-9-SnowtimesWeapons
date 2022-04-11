6a03cf168421b66c3a70009588c6d7aff2892fc1
Fesiug <publicfesiug@outlook.com>
2022-04-11 15:16:03 -0400

    don't freeze when opening menu

--- lua/arc9/common/attachments_bulk/snow_halo_skins.lua
+++ lua/arc9/common/attachments_bulk/snow_halo_skins.lua
@@ -32,9 +32,31 @@ for _, soup in ipairs(fileslookedup) do
 	ATT.CompactName = override.CompactName or coolname
 	ATT.Description = override.Description or soup
 	ATT.MenuCategory = override.MenuCategory or "ARC-9 - Camos"
-	ATT.Folder = override.Folder or nil
+
+	local shitcode
+	if !override.Folder then
+		local ser = tonumber(coolname)
+		if ser <= 100 then
+			shitcode = "0-100"
+		elseif ser <= 200 then
+			shitcode = "101-200"
+		elseif ser <= 300 then
+			shitcode = "201-300"
+		elseif ser <= 400 then
+			shitcode = "301-400"
+		elseif ser <= 500 then
+			shitcode = "401-500"
+		elseif ser <= 600 then
+			shitcode = "501-600"
+		elseif ser <= 700 then
+			shitcode = "601-700"
+		elseif ser <= 800 then
+			shitcode = "701-800"
+		end
+	end
+	ATT.Folder = override.Folder or shitcode
 	ATT.Category = override.Category or "universal_camo"
-	
+
 	ATT.SortOrder = tonumber(coolname)
 	ATT.Icon = Material("snowysnowtime/camos/" .. soup)
 	ATT.CustomCamoTexture = "snowysnowtime/camos/" .. soup
