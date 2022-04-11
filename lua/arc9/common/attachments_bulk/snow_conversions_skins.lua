41b12319b9562a1090c64b980de6d0f1dfe7fd7e
Snowy Snowtime <snowysnowtime1@gmail.com>
2022-04-11 12:23:40 -0700

    Bring back the conversions

---
+++ lua/arc9/common/attachments_bulk/snow_conversions_skins.lua
@@ -0,0 +1,151 @@
+-- Halo CE
+	-- Assault Rifle
+local ATT = {}
+
+ATT.PrintName = "Gold"
+ATT.CompactName = "GOLD"
+ATT.FullColorIcon = true
+ATT.Description = [[We call it gold, but its actually coated with Titanium Nitride.]]
+ATT.Icon = Material("snowysnowtime/camos/camo1")
+ATT.Category = "halo_skins"
+ATT.Folder = "Weapon Skins"
+
+ARC9.LoadAttachment(ATT, "skin_cear1")
+
+ATT = {}
+
+ATT.PrintName = "The Great Journey"
+ATT.CompactName = "GREAT"
+ATT.FullColorIcon = true
+ATT.Description = [["... and you shall be set loose against this heresy with our blessing."]]
+ATT.Icon = Material("snowysnowtime/camos/camo1")
+ATT.Category = "halo_skins"
+ATT.Folder = "Weapon Skins"
+
+ARC9.LoadAttachment(ATT, "skin_cear2")
+
+ATT = {}
+
+ATT.PrintName = "Lucky Shot"
+ATT.CompactName = "LUCK"
+ATT.FullColorIcon = true
+ATT.Description = [["They let me pick. Did I ever tell you that? Choose whichever Spartan I wanted. You know me. I did my research, watched as you became the soldier we needed you to be... Like the others, you were strong; and swift and brave, a natural leader. But you had something they didn't. Something no one saw; but me. Can you guess?"
+"Luck."
+- Cortana]]
+ATT.Icon = Material("snowysnowtime/camos/camo1")
+ATT.Category = "halo_skins"
+ATT.Folder = "Weapon Skins"
+
+ARC9.LoadAttachment(ATT, "skin_cear3")
+
+ATT = {}
+
+ATT.PrintName = "Squirt Gun"
+ATT.CompactName = "SQUIRT"
+ATT.FullColorIcon = true
+ATT.Description = [[Not sure why you'd use this thing, Chief. It's a straight downgrade!]]
+ATT.Icon = Material("snowysnowtime/camos/camo1")
+ATT.Category = "halo_skins"
+ATT.Folder = "Weapon Skins"
+
+ARC9.LoadAttachment(ATT, "skin_cear4")
+
+ATT = {}
+
+ATT.PrintName = "Desert"
+ATT.CompactName = "DESERT"
+ATT.FullColorIcon = true
+ATT.Description = [["...the corps issued me a rifle, not wings."]]
+ATT.Icon = Material("snowysnowtime/camos/camo1")
+ATT.Category = "halo_skins"
+ATT.Folder = "Weapon Skins"
+
+ARC9.LoadAttachment(ATT, "skin_cear5")
+
+ATT = {}
+
+ATT.PrintName = "Woodland"
+ATT.CompactName = "WOOD"
+ATT.FullColorIcon = true
+ATT.Description = [[The rifle has a note on it.
+"Radio for VTOL, heavy lift gear. We're not leaving him here."
+"Yeah... you're not."
+"Crazy fool, why do you always jump!? One of these days, you're gonna land on something as stubborn as you are... and I don't do bits and pieces."
+"Where is she Chief? Where's Cortana?"
+"...She stayed behind."
+- Sgt. Johnson & Master Chief]]
+ATT.Icon = Material("snowysnowtime/camos/camo1")
+ATT.Category = "halo_skins"
+ATT.Folder = "Weapon Skins"
+
+ARC9.LoadAttachment(ATT, "skin_cear6") 
+
+-- Titanfall 2/Apex Legends 
+	--G2A5
+local ATT = {}
+
+ATT.PrintName = "G7 Scout"
+ATT.FullColorIcon = true
+ATT.CompactName = "G7 Scout"
+-- ATT.Model = "models/snowysnowtime/arc9/tf2/rifles/c_g7scout.mdl"
+-- ATT.Bonemerge = true
+ATT.Icon = Material("snowysnowtime/camos/camo_1")
+ATT.Category = "apex_g2"
+ATT.Description = [[A later variant of the renowned G2A5 which was simplified to be mass produced.
+
+This conversion supports the usage of Camos.]]
+ATT.MenuCategory = "ARC-9 - Apex Legends"
+
+ARC9.LoadAttachment(ATT, "tfg2_apex01")
+
+ATT = {}
+
+ATT.PrintName = "G2A5 Mastery"
+ATT.FullColorIcon = true
+ATT.CompactName = "Mastery"
+-- ATT.Model = "models/snowysnowtime/arc9/tf2/rifles/c_g7scout.mdl"
+-- ATT.Bonemerge = true
+ATT.Icon = Material("snowysnowtime/camos/camo_1")
+ATT.Category = "apex_g2"
+ATT.Description = [[A variant of the G2A5 meant for the most elite of the elite.
+If you have this, you are a master G2 user.
+
+This Mastery skin does not support Camos.]]
+ATT.MenuCategory = "ARC-9 - Apex Legends"
+
+ARC9.LoadAttachment(ATT, "tfg2_apex02")
+
+	--R97
+
+ATT = {}
+
+ATT.PrintName = "R99"
+ATT.FullColorIcon = true
+ATT.CompactName = "R99"
+ATT.Model = "models/snowysnowtime/arc9/tf2/rifles/c_r97_apex.mdl"
+ATT.Bonemerge = true
+ATT.Icon = Material("snowysnowtime/camos/camo_1")
+ATT.Category = "apex_r97"
+ATT.Description = [[A successor to the popular R-97CN.
+
+This conversion supports the usage of Camos.]]
+ATT.MenuCategory = "ARC-9 - Apex Legends"
+
+ARC9.LoadAttachment(ATT, "tfr97_apex01")
+
+ATT = {}
+
+ATT.PrintName = "R99 Rule of Law"
+ATT.FullColorIcon = true
+ATT.CompactName = "R99-Law"
+ATT.Model = "models/snowysnowtime/arc9/tf2/rifles/c_r97_legendary06_apex.mdl"
+ATT.Bonemerge = true
+ATT.Icon = Material("snowysnowtime/camos/camo_1")
+ATT.Category = "apex_r97"
+ATT.Description = [[A successor to the popular R-97CN.
+This particular conversion gives the R99 a western appearance.
+
+This conversion does NOT support the usage of Camos.]]
+ATT.MenuCategory = "ARC-9 - Apex Legends"
+
+ARC9.LoadAttachment(ATT, "tfr97_apex02")
--- lua/arc9/common/attachments_bulk/snow_halo_skins.lua
+++ lua/arc9/common/attachments_bulk/snow_halo_skins.lua
@@ -8,10 +8,189 @@ local lookup = {
 		CompactName = "PAINBRUH",
 		Description = "Woah!!"
 	},
+	-- Flat Colors
 	["001"] = {
-		PrintName = "Piss Yeller",
-		CompactName = "PISSYELL",
-		Description = "Swoosh!!!!!!"
+		-- PrintName = "Yellow",
+		-- CompactName = "yello",
+		Description = "A flat color from Titanfall 2 & Apex Legends",
+		Folder = "TF|2/FLAT"
+	},
+	["002"] = {
+		Description = "A flat color from Titanfall 2 & Apex Legends",
+		Folder = "TF|2/FLAT"
+	},
+	["003"] = {
+		Description = "A flat color from Titanfall 2 & Apex Legends",
+		Folder = "TF|2/FLAT"
+	},
+	["004"] = {
+		Description = "A flat color from Titanfall 2 & Apex Legends",
+		Folder = "TF|2/FLAT"
+	},
+	["005"] = {
+		Description = "A flat color from Titanfall 2 & Apex Legends",
+		Folder = "TF|2/FLAT"
+	},
+	["006"] = {
+		Description = "A flat color from Titanfall 2 & Apex Legends",
+		Folder = "TF|2/FLAT"
+	},
+	["007"] = {
+		Description = "A flat color from Titanfall 2 & Apex Legends",
+		Folder = "TF|2/FLAT"
+	},
+	["008"] = {
+		Description = "A flat color from Titanfall 2 & Apex Legends",
+		Folder = "TF|2/FLAT"
+	},
+	["009"] = {
+		Description = "A flat color from Titanfall 2 & Apex Legends",
+		Folder = "TF|2/FLAT"
+	},
+	["010"] = {
+		Description = "A flat color from Titanfall 2 & Apex Legends",
+		Folder = "TF|2/FLAT"
+	},
+	["011"] = {
+		Description = "A flat color from Titanfall 2 & Apex Legends",
+		Folder = "TF|2/FLAT"
+	},
+	["012"] = {
+		Description = "A flat color from Titanfall 2 & Apex Legends",
+		Folder = "TF|2/FLAT"
+	},
+	["013"] = {
+		Description = "A flat color from Titanfall 2 & Apex Legends",
+		Folder = "TF|2/FLAT"
+	},
+	["014"] = {
+		Description = "A flat color from Titanfall 2 & Apex Legends",
+		Folder = "TF|2/FLAT"
+	},
+	["015"] = {
+		Description = "A flat color from Titanfall 2 & Apex Legends",
+		Folder = "TF|2/FLAT"
+	},
+	["016"] = {
+		Description = "A flat color from Titanfall 2 & Apex Legends",
+		Folder = "TF|2/FLAT"
+	},
+	["017"] = {
+		Description = "A flat color from Titanfall 2 & Apex Legends",
+		Folder = "TF|2/FLAT"
+	},
+	["018"] = {
+		Description = "A flat color from Titanfall 2 & Apex Legends",
+		Folder = "TF|2/FLAT"
+	},
+	["019"] = {
+		Description = "A flat color from Titanfall 2 & Apex Legends",
+		Folder = "TF|2/FLAT"
+	},
+	["020"] = {
+		Description = "A flat color from Titanfall 2 & Apex Legends",
+		Folder = "TF|2/FLAT"
+	},
+	["021"] = {
+		Description = "A flat color from Titanfall 2 & Apex Legends",
+		Folder = "TF|2/FLAT"
+	},
+	["022"] = {
+		Description = "A flat color from Titanfall 2 & Apex Legends",
+		Folder = "TF|2/FLAT"
+	},
+	["023"] = {
+		Description = "A flat color from Titanfall 2 & Apex Legends",
+		Folder = "TF|2/FLAT"
+	},
+	["024"] = {
+		Description = "A flat color from Titanfall 2 & Apex Legends",
+		Folder = "TF|2/FLAT"
+	},
+	["004"] = {
+		Description = "A flat color from Titanfall 2 & Apex Legends",
+		Folder = "TF|2/FLAT"
+	},
+	["025"] = {
+		Description = "A flat color from Titanfall 2 & Apex Legends",
+		Folder = "TF|2/FLAT"
+	},
+	["026"] = {
+		Description = "A flat color from Titanfall 2 & Apex Legends",
+		Folder = "TF|2/FLAT"
+	},
+	["027"] = {
+		Description = "A flat color from Titanfall 2 & Apex Legends",
+		Folder = "TF|2/FLAT"
+	},
+	["028"] = {
+		Description = "A flat color from Titanfall 2 & Apex Legends",
+		Folder = "TF|2/FLAT"
+	},
+	["029"] = {
+		Description = "A flat color from Titanfall 2 & Apex Legends",
+		Folder = "TF|2/FLAT"
+	},
+	["030"] = {
+		Description = "A flat color from Titanfall 2 & Apex Legends",
+		Folder = "TF|2/FLAT"
+	},
+	["031"] = {
+		Description = "A flat color from Titanfall 2 & Apex Legends",
+		Folder = "TF|2/FLAT"
+	},
+	["032"] = {
+		Description = "A flat color from Titanfall 2 & Apex Legends",
+		Folder = "TF|2/FLAT"
+	},
+	["033"] = {
+		Description = "A flat color from Titanfall 2 & Apex Legends",
+		Folder = "TF|2/FLAT"
+	},
+	["034"] = {
+		Description = "A flat color from Titanfall 2 & Apex Legends",
+		Folder = "TF|2/FLAT"
+	},
+	["035"] = {
+		Description = "A flat color from Titanfall 2 & Apex Legends",
+		Folder = "TF|2/FLAT"
+	},
+	["036"] = {
+		Description = "A flat color from Titanfall 2 & Apex Legends",
+		Folder = "TF|2/FLAT"
+	},
+	["037"] = {
+		Description = "A flat color from Titanfall 2 & Apex Legends",
+		Folder = "TF|2/FLAT"
+	},
+	["038"] = {
+		Description = "A flat color from Titanfall 2 & Apex Legends",
+		Folder = "TF|2/FLAT"
+	},
+	["039"] = {
+		Description = "A flat color from Titanfall 2 & Apex Legends",
+		Folder = "TF|2/FLAT"
+	},
+	["223"] = {
+		Description = "A flat color from Titanfall 2 & Apex Legends",
+		Folder = "TF|2/FLAT"
+	},
+	["326"] = {
+		Description = "A flat color from Titanfall 2 & Apex Legends",
+		Folder = "TF|2/FLAT"
+	},
+	-- Dev Textures
+	["650"] = {
+		PrintName = "Titanfall Dev Texture",
+		CompactName = "dev",
+		Description = "Why are you still here, this is a TEMP camo texture, David!",
+		Folder = "TF|2/DEV"
+	},
+	["651"] = {
+		PrintName = "Missing Texture",
+		CompactName = "#missing ",
+		Description = "<missing string>",
+		Folder = "TF|2/DEV"
 	},
 }
 
